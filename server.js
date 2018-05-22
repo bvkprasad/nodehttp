0a4fac0d35699c889ac34763afe608edfca878d5
bvkprasad9 <prasadbulusu9@gmail.com>
2018-05-23 00:45:36 +0530

    proper url query parsing

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const http = require("http");
 const url = require('url');
 const port = process.env.PORT || 80;
 const server = http.createServer((req,res)=>{
-    if(req.url === "/"){
-        res.writeHead(200,{'content-type':'text/html'});
-        var q = url.parse(req.url,true).query;
-        var name = q.name;
-        res.write("hello "+name);
-        res.end();
-    }
+    res.writeHead(200,{'content-type':'text/html'});
+    var q = url.parse(req.url,true).query;
+    var name = q.name;
+    res.write("hello "+name);
+    res.end();
 });
 
 server.listen(port);
