7007726a193305e20f08035800ee5ee8ed8abefc
bvkprasad9 <prasadbulusu9@gmail.com>
2018-05-23 00:20:36 +0530

    url parameters

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require("http");
+const url = require('url');
 const port = process.env.PORT || 80;
 const server = http.createServer((req,res)=>{
-    if(req.url === "/hi"){
-        res.write("hello world");
+    if(req.url === "/"){
+        res.writeHead(200,{'content-type':'text/html'});
+        var q = url.parse(req.url,true).query;
+        var name = q.name;
+        res.write("hello "+name);
         res.end();
     }
 });
